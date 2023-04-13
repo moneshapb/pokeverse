c5b4bdb7eb9d554805c3a1a311ea1770cf0f94f2
moneshapb <120640597+moneshapb@users.noreply.github.com>
2023-04-13 16:35:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Navigation } from './components/Navigation';
+import Container  from 'react-bootstrap/Container';
 import { PokemonCard } from './components/PokemonCard';
+import { Alert } from 'react-bootstrap';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
 
+
+const [pokemonList, setPokemonList] = useState([]);
+const [error, setError] = useState(false);
+
+
+  useEffect(() => {
+       getData();
+    async function getData() {
+      try {
+        const response = await fetch(pokeApi);
+      const data = await response.json();
+      setPokemonList(data.results);
+        } catch {
+        setError(true);
+      }
+   
+  }
+
+}, []);
+
+
+if(error){
+  return (
+    <div data-testid="app">
+    <Navigation />
+    <Container>
+    <Alert variant="danger">
+      Error: There was a problem fetching the pokemon data. 
+    </Alert>
+    </Container>
+
+  </div>
+  );
+}
+
+if(!pokemonList.length){
+  return (
+    <div data-testid="app">
+    <Navigation />
+    <Container>
+    <Alert variant="info">
+      Fetching the pokemon data... 
+    </Alert>
+    </Container>
+
+  </div>
+  );
+}
   return (
     <div data-testid="app">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <Container>
+{pokemonList.map(pokemon => <PokemonCard key={pokemon.name} {...pokemon} />)}      </Container>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Card} from 'react-bootstrap/Card';
+import { Alert } from 'react-bootstrap';
 
 function PokemonCard({ url, name }) {
+const [pokemon, setPokemon] = useState(null);
+const [error, setError] = useState(false);
 
+
+  useEffect(() => {
+       getData();
+    async function getData() {
+      try {
+        const response = await fetch(url);
+      const data = await response.json();
+      setPokemon({ abilities: data.abilities,
+        sprites: data.sprites });
+        } catch {
+        setError(true);
+      }
+   
+  }
+
+}, [url]);
+
+if (error) {
   return (
-    <div>
-        pokemon card
-    </div>
+    <Card>
+      <Card.Body>
+        <Alert variant="danger"> 
+        <Card.Title>Error</Card.Title>
+        <Card.Text>
+        Error: There was a problem fetching this pokemon.
+        </Card.Text>
+        </Alert>
+      </Card.Body>
+    </Card>
   );
+}
+
+if (!pokemon) {
+  return (
+    <Card>
+      <Card.Body>
+        <Alert variant="info"> 
+        <Card.Title>Loading...</Card.Title>
+        <Card.Text>
+        Fetching Pokemon Data...
+        </Card.Text>
+        </Alert>
+      </Card.Body>
+    </Card>
+  );
+}
+  return (
+    <Card>
+      <Card.Img variant="top" src={pokemon.sprites.front_default}/>
+      <Card.Body>
+        <Card.Title></Card.Title>
+        <Card.Text>          
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+ 
 }
 
 export { PokemonCard };
